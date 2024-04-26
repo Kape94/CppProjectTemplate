a7430bd6e50e4ed8fd387d16d332b8f349ff0b12
Kléber Kapelinski <kleberkapelinski@gmail.com>
2024-04-26 11:10:32 -0300

    Initial commit.

---
+++ Build.bat
@@ -0,0 +1,10 @@
+SET CMAKE=vcpkg/downloads/tools/cmake-3.27.1-windows/cmake-3.27.1-windows-i386/bin/cmake.exe
+SET VCPKG_TOOLCHAIN=vcpkg/scripts/buildsystems/vcpkg.cmake
+
+mkdir BUILD
+cd BUILD
+
+%CMAKE% .. -DCMAKE_TOOLCHAIN_FILE=../%VCPKG_TOOLCHAIN%
+%CMAKE% --build . 
+
+pause
---
+++ Install.bat
@@ -0,0 +1,10 @@
+git clone https://github.com/Microsoft/vcpkg.git
+
+cd vcpkg
+
+call bootstrap-vcpkg.bat
+
+# install the packages for you project
+# ex: vcpkg install glfw3
+
+pause
