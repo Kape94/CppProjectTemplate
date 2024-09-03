a5cc8150b614880c9e4f91ed084db2078f8fc77e
Kléber Kapelinski <kleberkapelinski@gmail.com>
2024-09-03 16:49:11 -0300

    Keep only one build script + add gitignore

--- Build.bat
+++ Build.bat
@@ -1,3 +1,22 @@
+@echo off
+setlocal
+
+:: 1) Setup vcpkg 
+git clone https://github.com/Microsoft/vcpkg.git
+
+cd vcpkg
+
+call bootstrap-vcpkg.bat
+
+:: 2) Install dependencies
+
+vcpkg install vcpkg-cmake
+vcpkg install vcpkg-cmake-config
+:: install the packages for you project
+:: ex: vcpkg install glfw3
+
+:: 3) Build the project
+cd ..
 SET CMAKE=""
 
 for /f "delims=" %%i in ('dir /s /b cmake.exe 2^>nul') do (
